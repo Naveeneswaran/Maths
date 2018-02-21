f8a0cc8a3460bba4b4120c2564ca6bf139cf12aa
naveen eswaran <naveeeneswarann@gmail.com>
2018-02-21 18:32:55 +0530

    First commit

---
+++ Codee/src/calc.java
@@ -0,0 +1,14 @@
+
+public class calc {
+	
+	public int add(int num1 ,int num2)
+	{
+		int r = num1 +num2;
+		return r;
+	}
+	public int sub(int num1,int num2) 
+	{
+		int r=  num1-num2;
+		return r;
+	}
+}
